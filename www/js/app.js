f4a590789f50fc677d98fd9b1bdfc9a7383734e1
jolitinh <jorge.hassan@tstllc.net>
2016-03-22 18:06:17 -0400

    some cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,7 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'ngCordova', 'IonicExercise.controllers', 'IonicExercise.services', 'IonicExercise.directives'])
 
 .run(function($ionicPlatform, $cordovaDevice) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
@@ -25,13 +18,7 @@ angular.module('starter', ['ionic', 'ngCordova', 'IonicExercise.controllers', 'I
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
-  // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
     abstract: true,
@@ -58,7 +45,7 @@ angular.module('starter', ['ionic', 'ngCordova', 'IonicExercise.controllers', 'I
     }
   });
 
-  // if none of the above states are matched, use this as the fallback
+  
   $urlRouterProvider.otherwise('/tab/images');
 
 });
